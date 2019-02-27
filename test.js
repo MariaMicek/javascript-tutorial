1d83021c0f05d0419b1fd8f176b7f679c375f180
Maria Micek <mmicek16@wp.pl>
2019-02-28 00:36:21 +0100

    break and continue

--- test.js
+++ test.js
@@ -1,7 +1,51 @@
-var links = document.getElementsByTagName("a");
+//Wyświetla liczby od 0 do 9
+for (i = 0; i <10; i++) {
 
-for (i = 1; i <= links.length; i++) {
-    console.log("This is link numer" + i);
+    console.log(i);
+    
+}
+console.log(".");
+
+
+//Przerywa po 7
+for (i = 0; i <10; i++) {
+
+    console.log(i);
+    
+    if (i === 7) {
+        break;
+    }
 }
 
-document.write("All links looped");
+console.log("I have broken out of the loop");
+console.log(".");
+
+
+//Omija 3 i 5
+for (i = 0; i <10; i++) {
+
+    if (i === 3 || i === 5) {
+        continue;
+    }
+
+    console.log(i);
+    
+}
+console.log(".");
+
+
+//Połączenie break i continue
+for (i = 0; i <10; i++) {
+
+    if (i === 3 || i === 5) {
+        continue;
+    }
+
+    console.log(i);
+    
+    if (i === 7) {
+        break;
+    }
+}
+
+console.log("I have broken out of the loop");
