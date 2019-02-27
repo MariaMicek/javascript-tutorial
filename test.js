1e4fc6b5dd30ff038f972303d7140eac50f3f626
Maria Micek <mmicek16@wp.pl>
2019-02-27 20:22:11 +0100

    Booleans, if, else, if else

--- test.js
+++ test.js
@@ -1,6 +1,31 @@
-alert("Hello World!");
+var youLikeMeat = false;
+if (youLikeMeat) {
+    document.write("Here is your meaty menu...");
+} else { (youLikeMeat)
+    document.write("Here is your vegan menu...");
+}
 
-var myVar = "Marysia";
-var myVar2 = 10;
-myVar2 = myVar2 - 3;
-var iLikeMeat = false;
+if (7>5) {
+    document.write("This is true...")
+}
+
+var myNumber = 10;
+if (myNumber < 8) {
+    document.write("My number is smaller than 8.")
+} else { (myNumber < 8)
+    document.write("My number is greater than 8.")
+}
+
+var myAge = 21;
+if (myAge > 30) {
+    document.write("You are over 30.")
+}
+else if (myAge > 20) {
+    document.write("You are over 20.")
+}
+else if (myAge > 10) {
+    document.write("You are over 10.")
+}
+else {
+    document.write("You are not over 10.")
+}
