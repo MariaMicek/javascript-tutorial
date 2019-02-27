e39d7fff404d5dc53ed68aa013f869e31e2a4999
Maria Micek <mmicek16@wp.pl>
2019-02-27 23:36:31 +0100

    Logical operators && ||

--- test.js
+++ test.js
@@ -1,31 +1,43 @@
 var youLikeMeat = false;
 if (youLikeMeat) {
-    document.write("Here is your meaty menu...");
+    document.write("Here is your meaty menu...  ");
 } else { (youLikeMeat)
-    document.write("Here is your vegan menu...");
+    document.write("Here is your vegan menu...  ");
 }
 
 if (7>5) {
-    document.write("This is true...")
+    document.write("This is true.  ");
 }
 
 var myNumber = 10;
 if (myNumber < 8) {
-    document.write("My number is smaller than 8.")
-} else { (myNumber < 8)
-    document.write("My number is greater than 8.")
+    document.write("My number is smaller than 8.  ");
+} else { (myNumber > 8)
+    document.write("My number is greater than 8.  ");
 }
 
-var myAge = 21;
+var myAge = 25;
 if (myAge > 30) {
-    document.write("You are over 30.")
+    document.write("You are over 30.  ");
 }
 else if (myAge > 20) {
-    document.write("You are over 20.")
+    document.write("You are over 20.  ");
 }
 else if (myAge > 10) {
-    document.write("You are over 10.")
+    document.write("You are over 10.  ");
 }
 else {
-    document.write("You are not over 10.")
+    document.write("You are not over 10.  ");
+}
+
+if (myAge >= 18 && myAge <= 30) {
+    document.write("You can come!  ");
+} else {
+    document.write("You can't come...  ");
+}
+
+if (myAge < 18 || myAge > 30) {
+    document.write("I said: You can't come...  ");
+} else {
+    document.write("I said: You can come!  ");
 }
