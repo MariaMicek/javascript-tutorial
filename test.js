0237a9743f9cf43226adf8eb74c98781eb7d60dc
Maria Micek <mmicek16@wp.pl>
2019-02-28 00:19:24 +0100

    for loop, example with links

--- test.js
+++ test.js
@@ -1,54 +1,7 @@
-var youLikeMeat = false;
-if (youLikeMeat) {
-    document.write("Here is your meaty menu...  ");
-} else { (youLikeMeat)
-    document.write("Here is your vegan menu...  ");
+var links = document.getElementsByTagName("a");
+
+for (i = 1; i <= links.length; i++) {
+    console.log("This is link numer" + i);
 }
 
-if (7>5) {
-    document.write("This is true.  ");
-}
-
-var myNumber = 10;
-if (myNumber < 8) {
-    document.write("My number is smaller than 8.  ");
-} else { (myNumber > 8)
-    document.write("My number is greater than 8.  ");
-}
-
-var myAge = 25;
-if (myAge > 30) {
-    document.write("You are over 30.  ");
-}
-else if (myAge > 20) {
-    document.write("You are over 20.  ");
-}
-else if (myAge > 10) {
-    document.write("You are over 10.  ");
-}
-else {
-    document.write("You are not over 10.  ");
-}
-
-if (myAge >= 18 && myAge <= 30) {
-    document.write("You can come!  ");
-} else {
-    document.write("You can't come...  ");
-}
-
-if (myAge < 18 || myAge > 30) {
-    document.write("I said: You can't come...  ");
-} else {
-    document.write("I said: You can come!  ");
-}
-
-while (myAge < 30) {
-    console.log("You are less than 30");
-    myAge++;
-}
-document.write("You are over 30 now!  ");
-
-for (age= 5; age < 10; age++) {
-    console.log("You are less than 10");
-}
-document.write("You are over 10 now!  ");
+document.write("All links looped");
