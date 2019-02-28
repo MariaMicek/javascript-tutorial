571e7a217b5985a454dfba57a9007d9c0efd6389
Maria Micek <mmicek16@wp.pl>
2019-02-28 19:54:41 +0100

    function, return

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-var links = document.getElementsByTagName("a");
-
-for (i = 0; i < links.length; i++) {
-    links[i].className = "link-" + i;
+function myNumber() {
+    console.log("My favourite number is:");
 }
+myNumber();
+
+
+function getAverage(a,b) {
+    var average = (a + b) / 2;
+    console.log(average);
+}
+getAverage(14,18);
+
+//Przypisanie wyniku funkcji do nowej zmiennej
+function getAverage2(a,b,c,d,e) {
+    var average2 = (a + b + c + d + e) / 5;
+    console.log(average2);
+    return average2;
+}
+
+var myResult = getAverage2(3,16,2,8,65);
+console.log("The average is " + myResult);
+
+
