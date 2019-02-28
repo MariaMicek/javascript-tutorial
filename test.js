f7f301d3a363ef386c2c549cbfded5958770739e
Maria Micek <mmicek16@wp.pl>
2019-02-28 01:06:02 +0100

    Giving classes to <a> tags with for loop

--- test.js
+++ test.js
@@ -1,51 +1,5 @@
-//Wyświetla liczby od 0 do 9
-for (i = 0; i <10; i++) {
+var links = document.getElementsByTagName("a");
 
-    console.log(i);
-    
+for (i = 0; i < links.length; i++) {
+    links[i].className = "link-" + i;
 }
-console.log(".");
-
-
-//Przerywa po 7
-for (i = 0; i <10; i++) {
-
-    console.log(i);
-    
-    if (i === 7) {
-        break;
-    }
-}
-
-console.log("I have broken out of the loop");
-console.log(".");
-
-
-//Omija 3 i 5
-for (i = 0; i <10; i++) {
-
-    if (i === 3 || i === 5) {
-        continue;
-    }
-
-    console.log(i);
-    
-}
-console.log(".");
-
-
-//Połączenie break i continue
-for (i = 0; i <10; i++) {
-
-    if (i === 3 || i === 5) {
-        continue;
-    }
-
-    console.log(i);
-    
-    if (i === 7) {
-        break;
-    }
-}
-
-console.log("I have broken out of the loop");
