3ea2b16e82d8778a7a7b76210738c5b94587feed
Maria Micek <mmicek16@wp.pl>
2019-03-01 18:50:09 +0100

    Date object

--- test.js
+++ test.js
@@ -1,23 +1,52 @@
-//constructor function
-var Car = function(maxSpeed, driver) {
+//DATE OBJECT
+//obecna data:
+var currentDate = new Date();
+console.log(currentDate);
 
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log("How many miles? ");
-        console.log(speed * time);
-    };
-    this.logDriver = function() {
-        console.log("The name of the driver is " + this.driver);
-    };
+//dowolna data
+var pastDate = new Date(1410, 0, 13, 23, 50, 34);
+var futureDate = new Date(2415, 11, 24, 3, 0, 59);
+console.log(pastDate);
+console.log(futureDate);
+
+//methods
+console.log(pastDate.getMonth());
+console.log(pastDate.getFullYear());
+console.log(pastDate.getDate());
+console.log(pastDate.getDay());
+console.log(pastDate.getHours());
+console.log(pastDate.getTime()); //milisekundy od 01.01.1970
+
+//porównywanie dat za pomocą .getTime
+if (pastDate.getTime() == futureDate.getTime()) {
+    console.log("Dates are equal.");
+} else {
+    console.log("Dates are not equal.");
 };
 
-//objects based on constructor function
-var Car1 = new Car(35, "Basia");
-var Car2 = new Car(70, "Marysia");
-var Car3 = new Car(60, "Kasia");
+//ile czasu mieneło od moich urodzin?
+var now = new Date();
+var myBirthday = new Date(1998, 1, 16, 1, 30, 0);
 
-//calling some of the properties and functions from objects
-Car1.drive(30, 1.5);
-Car2.logDriver();
-console.log(Car3.maxSpeed, Car3.driver);
+//w latach:
+function Difference() {
+    var difference = (now.getTime() - myBirthday.getTime())/1000/60/60/24/365.25;
+    console.log(difference);
+    return difference;
+};
+var myAge = Difference();
+console.log("I was born " + myAge + " years ago.")
+
+
+//w dniach:
+var myAge = (now.getTime() - myBirthday.getTime())/1000/60/60/24;
+console.log("I was born " + myAge + " days ago.");
+
+
+//w milisekundach:
+console.log("I was born " + (now.getTime() - myBirthday.getTime()) + " miliseconds ago.");
+
+
+
+
+
