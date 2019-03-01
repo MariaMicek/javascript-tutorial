bb06390af3a2a1477876e8d516695b7ed7ef1dcb
Maria Micek <mmicek16@wp.pl>
2019-03-01 10:35:43 +0100

    slice and split method

--- test.js
+++ test.js
@@ -1,47 +1,3 @@
-function myNumber() {
-    console.log("My favourite number is:");
-}
-myNumber();
-
-
-function getAverage(a,b) {
-    var average = (a + b) / 2;
-    console.log(average);
-}
-getAverage(14,18);
-
-//Przypisanie wyniku funkcji do nowej zmiennej
-function getAverage2(a,b,c,d,e) {
-    var average2 = (a + b + c + d + e) / 5;
-    console.log(average2);
-    return average2;
-}
-
-var myResult = getAverage2(3,16,2,8,65);
-console.log("The average is " + myResult);
-
-
-//Not a number
-console.log(isNaN(2));
-
-var a = 12;
-var b = "banana";
-
-if (isNaN(b)) {
-    console.log("This aint a number...");
-} else {
-    console.log("Your lucky number is" + (a * b));
-}
-
-var aa = 12;
-var bb = 3;
-
-if (isNaN(bb)) {
-    console.log("This aint a number...");
-} else {
-    console.log("Your lucky number is " + (aa * bb));
-}
-
 //STRING
 var myString = "I\'m a \"fun\" ninja string";
 
@@ -63,3 +19,17 @@ var string1 = "abc";
 var string2 = "ABC";
 console.log(string1.toUpperCase() === string2); //true
 console.log(string1 < string2); //false
+
+//slice method
+var stringSlice = "hello world";
+var stringSlice2 = stringSlice.slice(2,8);
+var stringSlice3 = stringSlice.slice(3);
+console.log(stringSlice);
+console.log(stringSlice2);
+console.log(stringSlice3);
+
+//split method
+var tags = "banana, apple, orange, berry";
+var tagsArrey = tags.split(",");
+console.log(tags);
+console.log(tagsArrey);
