f75def3b0c9af60072897a76e83946e5e0b8f1af
Maria Micek <mmicek16@wp.pl>
2019-03-02 16:43:33 +0100

    DOM

--- test.js
+++ test.js
@@ -1,52 +1,45 @@
-//DATE OBJECT
-//obecna data:
-var currentDate = new Date();
-console.log(currentDate);
+//DOM
+var myContainer = document.getElementsByClassName("container");
+var myH2 = myContainer[0].getElementsByTagName("h2");
 
-//dowolna data
-var pastDate = new Date(1410, 0, 13, 23, 50, 34);
-var futureDate = new Date(2415, 11, 24, 3, 0, 59);
-console.log(pastDate);
-console.log(futureDate);
+//innerHTML
+var myBody = document.getElementsByTagName("body");
+console.log(myBody[0].innerHTML);
+myH2[0].innerHTML = "<h6> Bye Bye </h6>";
 
-//methods
-console.log(pastDate.getMonth());
-console.log(pastDate.getFullYear());
-console.log(pastDate.getDate());
-console.log(pastDate.getDay());
-console.log(pastDate.getHours());
-console.log(pastDate.getTime()); //milisekundy od 01.01.1970
+//textContent
+var myParagraph = document.getElementById("paragraph");
+console.log(myParagraph.textContent);
+myParagraph.textContent = "I am a sad paragraph...";
 
-//porównywanie dat za pomocą .getTime
-if (pastDate.getTime() == futureDate.getTime()) {
-    console.log("Dates are equal.");
-} else {
-    console.log("Dates are not equal.");
-};
+//changing element attribute
+var link = document.getElementById("link");
+console.log(link.getAttribute("id"));
+console.log(link.getAttribute("target"));
+console.log(link.setAttribute("class","blog"));
+console.log(link.className);
+console.log(link.className = "best-blog")
 
-//ile czasu mieneło od moich urodzin?
-var now = new Date();
-var myBirthday = new Date(1998, 1, 16, 1, 30, 0);
+//style attribute
+link.setAttribute("style", "fontSize:20px ; color:red");
+link.style.backgroundColor = "yellow";
+link.style.textDecoration = "none";
 
-//w latach:
-function Difference() {
-    var difference = (now.getTime() - myBirthday.getTime())/1000/60/60/24/365.25;
-    console.log(difference);
-    return difference;
-};
-var myAge = Difference();
-console.log("I was born " + myAge + " years ago.")
+//adding new elements
+var newLi = document.createElement("li");       //child element
+var menu = document.getElementById("nav").getElementsByTagName("ul")[0];    //parent element
+
+menu.appendChild(newLi);
+menu.insertBefore(newLi, menu.getElementsByTagName("li")[0]);
 
 
-//w dniach:
-var myAge = (now.getTime() - myBirthday.getTime())/1000/60/60/24;
-console.log("I was born " + myAge + " days ago.");
+var newA = document.createElement("a");     //child element
+newLi.appendChild(newA);
+newA.innerHTML = "contact me";
+newA.setAttribute("href", "#");
 
-
-//w milisekundach:
-console.log("I was born " + (now.getTime() - myBirthday.getTime()) + " miliseconds ago.");
-
-
-
-
-
+//removing elements 
+var parent = document.getElementById("nav").getElementsByTagName("ul")[0];
+var child = parent.getElementsByTagName("li")[1];
+var removed = parent.removeChild(child);
+parent.appendChild(removed);
