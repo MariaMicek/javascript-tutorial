ff48178d523fb389776b49063b5bb9a2d26fee32
Maria Micek <mmicek16@wp.pl>
2019-03-02 17:27:45 +0100

    Events

--- test.js
+++ test.js
@@ -1,45 +1,14 @@
-//DOM
-var myContainer = document.getElementsByClassName("container");
-var myH2 = myContainer[0].getElementsByTagName("h2");
-
-//innerHTML
-var myBody = document.getElementsByTagName("body");
-console.log(myBody[0].innerHTML);
-myH2[0].innerHTML = "<h6> Bye Bye </h6>";
-
-//textContent
-var myParagraph = document.getElementById("paragraph");
-console.log(myParagraph.textContent);
-myParagraph.textContent = "I am a sad paragraph...";
-
-//changing element attribute
-var link = document.getElementById("link");
-console.log(link.getAttribute("id"));
-console.log(link.getAttribute("target"));
-console.log(link.setAttribute("class","blog"));
-console.log(link.className);
-console.log(link.className = "best-blog")
-
-//style attribute
-link.setAttribute("style", "fontSize:20px ; color:red");
-link.style.backgroundColor = "yellow";
-link.style.textDecoration = "none";
-
-//adding new elements
-var newLi = document.createElement("li");       //child element
-var menu = document.getElementById("nav").getElementsByTagName("ul")[0];    //parent element
-
-menu.appendChild(newLi);
-menu.insertBefore(newLi, menu.getElementsByTagName("li")[0]);
+var title = document.getElementsByTagName("h1")[0];
 
 
-var newA = document.createElement("a");     //child element
-newLi.appendChild(newA);
-newA.innerHTML = "contact me";
-newA.setAttribute("href", "#");
+title.onclick = function () {
+    alert("you clicked me");
+};
 
-//removing elements 
-var parent = document.getElementById("nav").getElementsByTagName("ul")[0];
-var child = parent.getElementsByTagName("li")[1];
-var removed = parent.removeChild(child);
-parent.appendChild(removed);
+title.onmouseover = function () {
+    this.setAttribute("style", "color:red;");
+};
+
+title.onmouseout = function () {
+    this.setAttribute("style", "color:black;");
+}
