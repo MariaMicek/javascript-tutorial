761b21ccc97c5a09ed94e9c6a75250044696f6d2
Maria Micek <mmicek16@wp.pl>
2019-03-02 18:32:12 +0100

    window.onload

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents() {   
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
 
-button.onclick = function () {
- 
-if (content.className == "open") {
+    button.onclick = function () {
+    
+        if (content.className == "open") {
 
-    //shrink the box
-    content.className = "";
-    button.innerHTML = "Show more";
+            //shrink the box
+            content.className = "";
+            button.innerHTML = "Show more";
 
-} else {
+        } else {
 
-    //expand the box
-    content.className = "open";
-    button.innerHTML = "Show less";
+            //expand the box
+            content.className = "open";
+            button.innerHTML = "Show less";
 
+        };
+    };
 };
+
+window.onload = function () {
+    setUpEvents();
 };
