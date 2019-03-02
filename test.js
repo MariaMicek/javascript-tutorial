01e4ef91517bb5b0ec8fc773aca505c3afac5b28
Maria Micek <mmicek16@wp.pl>
2019-03-02 22:31:47 +0100

    jQuery

--- test.js
+++ test.js
@@ -1,33 +1,13 @@
-console.log(document.forms.myForm);
-var myForm = document.forms.myForm;
+//grabbing elements with DOM
+var myPara = document.getElementById("content").getElementsByTagName("p")[5];
 
-console.log(myForm.name);
-console.log(myForm.hobbies);
-console.log(myForm.color);
+//grabbing elements with jQuery
+var myParagraph = $("#content p:last-child");
 
-console.log(myForm.name.value);
-console.log(myForm.hobbies.value);
-console.log(myForm.color.value);
-
-//onfocus event
-myForm.name.onfocus = function() {
-    myForm.name.style.border = "4px solid pink";
-};
-
-//onblur event
-myForm.name.onblur = function() {
-    myForm.name.style.border = "none";
-};
-
-//onsubmit event
-var message = document.getElementById("message");
-myForm.onsubmit = function() {
-    if (myForm.name.value == "") {
-        message.innerHTML = "please enter your name";
-        return false;
-    } else {
-        message.innerHTML = "";
-        return true;
-    };
-};
-
+//methods in jQuery
+myParagraph.addClass("test");
+myParagraph.removeClass("test");
+myParagraph.fadeOut();
+myParagraph.fadeIn();
+myParagraph.css({color:"red", fontSize:"24px"});
+myParagraph.animate({bottom:"100px"});
